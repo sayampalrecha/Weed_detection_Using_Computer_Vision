4516412ec072bc2710268f70d9e58437d0796459
Sayam Palrecha <104487341+sayampalrecha@users.noreply.github.com>
2024-04-03 20:38:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ import os
 from PIL import Image
 
 st.title('🌱 Weed Detection')
-st.write('### This web app can detect whether a plant is a crop or weed.')
+st.write('### This web app can detect whether a plant is a weed.')
+st.write('carpetweeds crabgrass eclipta goosegrass morningglory nutsedge palmeramaranth pricklysida purslane ragweed sicklepod spottedspurge spurredanoda swinecress waterhemp')
 
 st.write('#### Upload an image.')
 uploaded_file = st.file_uploader('', type=['png', 'jpg', 'jpeg'], accept_multiple_files=False)
